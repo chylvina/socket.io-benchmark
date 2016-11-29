1d1cfbc804c77b27ea187eb94acfbe6b4c54b487
thiagoods <t.oliveira@outlook.com>
2016-11-29 19:27:01 -0200

    split benchmark for polling and ws

---
+++ wsbench.js
@@ -0,0 +1,41 @@
+var profile = require('v8-profiler');
+var io = require('socket.io-client');
+
+var message = "o bispo de constantinopla nao quer se desconstantinopolizar";
+
+var argvIndex = 2;
+var transport = 'websocket';
+var users = parseInt(process.argv[argvIndex++]);
+var rampUpTime = parseInt(process.argv[argvIndex++]) * 1000;
+var newUserTimeout = rampUpTime / users;
+var shouldBroadcast = process.argv[argvIndex++] === 'broadcast' ? true : false;
+var host = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : 'localhost';
+var port = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '3000';
+
+function user(transport, shouldBroadcast, host, port) {
+  var socket = io.connect('http://' + host + ':' + port, {'forceNew': true, transports: [transport]});
+
+  socket.on('connect', function() {
+    if (shouldBroadcast) {
+      socket.emit('broadcast', message);
+    } else {
+      socket.send(message);
+    }
+
+    socket.on('message', function(message) {
+      socket.send(message);
+    });
+
+    socket.on('broadcastOk', function() {
+      socket.emit('broadcast', message);
+    });
+
+    socket.once('disconnect', function() {
+      socket.connect();
+    });
+  });
+};
+
+for(var i=0; i<users; i++) {
+  setTimeout(function() { user(transport, shouldBroadcast, host, port); }, i * newUserTimeout);
+};
